04cd94717a7d047d04f509a8df542716f334ab17
Sokos6 <will@comicbooked.com>
2016-04-17 19:26:59 -0600

    changed loop to until and removed break. Created initial logic for user input, list, add, etc, and created the break label for exiting when typing quit.

--- lib/guide.rb
+++ lib/guide.rb
@@ -19,10 +19,30 @@ class Guide
     def launch!
         introduction
         # action loop
+        result = nil
+        until result == :quit
         # what do you want to do? (list, find, add, quit)
-        # do that action
-        # repeat until user quits
+            print "> "
+            user_response = gets.chomp
+            # do that action
+            result = do_action(user_response)
+        end
         conclusion
+    end
+    
+    def do_action(action)
+        case action
+        when 'list'
+            puts "Listing..."
+        when 'find'
+            puts 'Finding...'
+        when 'add'
+            puts "Adding..."
+        when 'quit'
+            return :quit
+        else
+            puts "\nI don't understand that command.\n"
+        end
     end
     
     def introduction
